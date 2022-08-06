1c1c2e5383497ddcd8fa7c9e92b92bcaca6ba987
ViperaSoft <110694127+ViperaSoft@users.noreply.github.com>
2022-08-06 05:20:16 +0300

    Update maps-viperasoft.com.py

--- maps-viperasoft.com.py
+++ maps-viperasoft.com.py
@@ -9,4 +9,28 @@ op.add_argument("--disable-dev-sh-usage")
 op.add_argument('--window-size=1920,1080')
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
 driver.get("https://www.google.com/maps")
-input("BİR TUŞA BASINIZ")
+
+time.sleep(10)
+
+username = driver.find_element_by_xpath("//*[@id='searchboxinput']")
+
+ara = "dişçi near istanbul"
+
+username.send_keys(ara)
+
+search_click.click()
+
+time.sleep(10)
+
+ilk_firma = driver.find_element_by_xpath("/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/div[" + str(sayac) + "]/div/a")
+
+time.sleep(10)
+
+firma_isim = driver.find_element_by_xpath("/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[3]/div/div[1]/div/div/div[2]/div[2]/div[1]/div[1]/div[1]/h1").text
+
+print(firma_isim)
+
+
+
+
+
