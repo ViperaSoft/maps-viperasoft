f32330d77904d8a6b3b83b4572db040db369a23c
ViperaSoft <110694127+ViperaSoft@users.noreply.github.com>
2022-08-06 05:58:28 +0300

    Update maps-viperasoft.com.py

--- maps-viperasoft.com.py
+++ maps-viperasoft.com.py
@@ -3,9 +3,9 @@ import time
 from selenium import webdriver
 op = webdriver.ChromeOptions()
 op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-#op.add_argument("--headless")
-#op.add_argument("--no-sandbox")
-#op.add_argument("--disable-dev-sh-usage")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("--disable-dev-sh-usage")
 op.add_argument('--window-size=1920,1080')
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
 driver.get("https://www.google.com/maps")
