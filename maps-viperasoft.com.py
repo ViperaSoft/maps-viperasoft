23ca505710733b28c5765d1a33570bcf07266c56
ViperaSoft <110694127+ViperaSoft@users.noreply.github.com>
2022-08-06 05:10:50 +0300

    Update maps-viperasoft.com.py

--- maps-viperasoft.com.py
+++ maps-viperasoft.com.py
@@ -1,27 +1,12 @@
 import os
 import time
-
 from selenium import webdriver
-
-
-timeout = 10
-
-sayac = 3
-
-firma_sayisi = 0
-
-
 op = webdriver.ChromeOptions()
 op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 op.add_argument("--headless")
 op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-sh-usage")
 op.add_argument('--window-size=1920,1080')
-
-
-
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
-
 driver.get("https://www.google.com/maps")
-
 input("BİR TUŞA BASINIZ"
